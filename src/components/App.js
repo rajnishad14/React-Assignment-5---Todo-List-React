fb97a0f3eb84b8ea9a3d003422295c42afe9a640
Raj Kumar Nishad <raj.nishad14@gmail.com>
2021-04-05 20:51:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,105 @@
-import React from "react";
-import "./../styles/App.css";
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './../styles/App.css'
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
+  const [edit, setEdit] = useState('')
+  const [isEdit, setIsEdit] = useState(false)
+  const [togg, setTogg] = useState(edit && true)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (task) {
+      const newTask = {
+        id: new Date().getTime().toString(),
+        task,
+        ed: false,
+      }
+      setTasks([...tasks, newTask])
+      setTask('')
+    }
+  }
+  const update = (id) => {
+    setIsEdit(false)
+    const newTasks = tasks
+    newTasks.forEach((ele) => {
+      if (ele.id === id) {
+        ele.task = edit
+        ele.ed = false
+      }
+    })
+    setTasks(newTasks)
+    setEdit('')
+  }
+  const change = (id) => {
+    setIsEdit(true)
+    const toEdit = tasks.find((ele) => ele.id === id)
+    toEdit.ed = true
+    setEdit(toEdit.task)
+  }
+  const remove = (id) => {
+    const newTasks = tasks.filter((ele) => ele.id !== id)
+    setTasks(newTasks)
+  }
+  useEffect(() => {
+    setTogg(edit ? false : true)
+  }, [edit])
+
+  return (
+    <div id="main">
+      <form>
+        <input
+          type="text"
+          name="task"
+          value={task}
+          id="task"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit" id="btn" onClick={(e) => handleSubmit(e)}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {tasks.map((ele) => {
+          const { id, task, ed } = ele
+          return (
+            <li key={id}>
+              {isEdit && ed ? (
+                <>
+                  <input
+                    className="editTask "
+                    type="text"
+                    value={edit}
+                    onChange={(e) => setEdit(e.target.value)}
+                  />
+                  <button
+                    className="saveTask "
+                    disabled={togg}
+                    onClick={() => update(id)}
+                  >
+                    Update
+                  </button>
+                </>
+              ) : (
+                <>
+                  {task}
+                  <button className="delete " onClick={() => remove(id)}>
+                    Delete
+                  </button>
+                  <button className="edit " onClick={() => change(id)}>
+                    Edit
+                  </button>
+                </>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
-
-export default App;
+export default App
